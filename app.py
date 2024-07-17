b561bcff3863fd6fad4a7c8b1930c974109dde13
Duresa Feyisa <101529984+duressa-feyissa@users.noreply.github.com>
2024-07-17 18:17:35 +0300

    Update app.py

--- app.py
+++ app.py
@@ -47,4 +47,5 @@ def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 8000))
+    app.run(host='0.0.0.0', port=port, debug=False)
